491cd0619c9ec5f4be18ad58a6638ca1ba3c2cb5
rajat844 <rajat230616@gmail.com>
2021-03-01 14:42:35 +0530

    5th done

--- Move all the negative elements to one side of the array/main.cpp
+++ Move all the negative elements to one side of the array/main.cpp
@@ -4,28 +4,22 @@ using namespace std;
 
 
 int main() {
-    int n;
-	cin>>n;
+	int n;
+	cin >> n;
 	int a[n];
-	for(int i{0};i<n;i++){
-	    cin>>a[i];
+	for (int i{ 0 };i < n;i++) {
+		cin >> a[i];
 	}
-	int mid=0;
-	int high = n-1;
 	int low = 0;
-	while(mid <= high){
-	    if(a[mid]<0){
-	        swap(a[mid++],a[low++]);
-	    }
-	    else if(a[mid]==0){
-	        mid++;
-	    }
-	    else if(a[mid]>0){
-	        swap(a[mid],a[high--]);
-	    }
+	for (int i = 0;i < n;i++) {
+		if (a[i] < 0) {
+			if (i != low)
+				swap(a[i], a[low]);
+			low++;
+		}
 	}
-	for(int i{0};i<n;i++){
-	    cout<<a[1]<<" ";
+	for (int i{ 0 };i < n;i++) {
+		cout << a[i] << " ";
 	}
 	return 0;
 }
